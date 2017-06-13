db481bcd137ed044c10c8f63fc504cb4c99e0f85
Izelaifi <izelaifi@adlere.fr>
2017-06-13 18:42:01 +0200

    IZI Exo name Space

---
+++ NameSpaceExo1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace SampleNameSpace
+{
+    class SampleClass
+    {
+        public void SampleMethod()
+        {
+            Console.WriteLine("SampleMethod inside SampleNameSpace");
+        }
+    }
+
+    namespace NestedNameSpace
+    {
+        class SampleClass
+        {
+            public void SampleMethod()
+            {
+                Console.WriteLine("SampleMethod inside NestedNameSpace");
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SampleClass outer = new SampleClass();
+            outer.SampleMethod();
+
+            SampleNameSpace.SampleClass outer2 = new SampleNameSpace.SampleClass();
+            outer2.SampleMethod();
+
+            NestedNameSpace.SampleClass inner = new NestedNameSpace.SampleClass();
+            inner.SampleMethod();
+
+        }
+    }
+}
+
+
---
+++ NameSpaceExo1/SampleClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleNameSpace
+{
+    class SampleClass
+    {
+        public void SampleMethode()
+        {
+            System.Console.WriteLine("SampleMethod inside SampleNameSpace");
+
+        }
+    }
+
+    namespace NestedNameSpace
+    {
+        class SampleClass
+        {
+            public void SampleMethode()
+            {
+                System.Console.WriteLine("SampleMethod inside NestedNameSpace");
+
+            }
+        }
+    }
+}
