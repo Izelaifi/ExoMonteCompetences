61974ee827f3c25985d0611f5820f9d164973423
zelaifi ismail <izelaifi@adlere.fr>
2017-06-16 16:24:28 +0200

    Exo avec la Poo, les enums, et les arraylist

---
+++ EnumExo2/ConnexionResult.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+
+
+namespace EnumExo2
+{
+    public class ConnexionResult
+    {
+        public enum ConnexionResultOprion
+        {
+            ConnexionOK,
+            WrongLogin,
+            Wrongpassword
+        }
+        public ConnexionResultOprion UserCanConnect(string login, string pass)
+        {
+            if (UserExsist(login))
+            {
+                if (LoginAndPasswordMatch(login, pass))
+                {
+                    return ConnexionResultOprion.ConnexionOK;
+                }
+                else
+                    return ConnexionResultOprion.Wrongpassword;
+            }
+            return ConnexionResultOprion.WrongLogin;
+        }
+        public static bool LoginAndPasswordMatch(string login, string pass)
+        {
+            string existPass;
+
+            ArrayList passtest = new ArrayList();
+            passtest.Add("toto");
+            passtest.Add("papa");
+            passtest.Add("Mama");
+
+            for (int i = 0; i < passtest.Count; i++)
+            {
+                existPass = passtest[i].ToString();
+                if (pass == existPass)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool UserExsist(string login)
+        {
+            string existLog;
+
+            ArrayList logintest = new ArrayList();
+            logintest.Add("toto");
+            logintest.Add("papa");
+            logintest.Add("Mama");
+
+            for (int i = 0; i < logintest.Count; i++)
+            {
+                existLog = logintest[i].ToString();
+                if (login == existLog)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ EnumExo2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnexionResult connexionResult = new ConnexionResult();
+            Console.WriteLine(connexionResult.UserCanConnect("toto", "toto"));
+            Console.ReadLine();
+        }
+    }
+}
